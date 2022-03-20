adeba98abf9a3ed1500fd3a1aaf722e75d67b191
wenychen <100336203+wenychen@users.noreply.github.com>
2022-03-20 19:46:51 +0800

    Add files via upload

--- loginfail.php
+++ loginfail.php
@@ -3,7 +3,7 @@
 
 <html>
 	<head>
-		<title>會員登入</title>
+		<title>會員登入失敗</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<meta name="description" content="NUKIM_php_homework">
@@ -19,7 +19,7 @@
 					<div class="container">
 
 						<!-- Nav -->
-						<h1 id="logo">會員登入</h1>
+						<h1 id="logo">會員登入失敗</h1>
 					</div>
 				</section>
 
@@ -37,24 +37,14 @@
 								<form action="" method="post" style="width: 60%; margin: 0px auto;" enctype="multipart/form-data">
 									<table>
 										<tr>
-											<img src="/php_homework/web.login&register/images/user.png" width="150px" height="150px">
+											<td><a href="login.php">重新登入</a></td>
+										</tr>
 										</tr>
 										<tr>
-											<td><input type="text" name="uaccount" style="text-align: center;" onkeyup="value=value.replace(/[^\w\.\/]/ig,'')" maxlength="20" pattern="[a-zA-Z0-9]{4}" placeholder="您的帳號" required="required"></td>
+											<td colspan="2"><a href="">忘記帳號與密碼</a></td>
 										</tr>
 										<tr>
-											<td><input type="password" name="upsw" style="text-align: center;" onkeyup="value=value.replace(/[^\w\.\/]/ig,'')" maxlength="20" pattern="[a-zA-Z0-9]{4}" placeholder="您的密碼" required="required"></td>
-										</tr>
-										<tr>
-											<td>
-												<input type="submit" name="" value="登入" style="width: 100%;">
-											</td>
-										</tr>
-										<tr>
-											<td><a href="">忘記帳號與密碼</a></td>
-										</tr>
-										<tr>
-											<td><a href="register.php">註冊去！</a></td>
+											<td colspan="2"><a href="register.php">註冊去！</a></td>
 										</tr>
 									</table>
 								</form>		
