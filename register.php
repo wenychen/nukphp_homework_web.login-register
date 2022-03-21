465b5106bc84cc0535ab29e13a944204d0e7aad5
wenychen <100336203+wenychen@users.noreply.github.com>
2022-03-21 10:01:42 +0800

    Update register.php

--- register.php
+++ register.php
@@ -7,7 +7,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<meta name="description" content="NUKIM_php_homework">
     	<meta name="author" content="weny">
-    	<link rel="icon" href="/php_homework/web.login&register/images/id-card.png" type="image/x-icon"/>
+    	<link rel="icon" href="/php_homework/web.login.register/images/id-card.png" type="image/x-icon"/>
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 		<link href='https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css' rel='stylesheet'></link>
 		<link rel="stylesheet" href="assets/css/main.css" />
